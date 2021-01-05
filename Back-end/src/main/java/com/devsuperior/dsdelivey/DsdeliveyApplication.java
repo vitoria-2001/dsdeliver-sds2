69738befe4e91b3509941a8cfa62164bd3135f14
Vitória Amorim <vitoria.amorimm01@gmail.com>
2021-01-05 13:28:27 -0400

    Project created

---
+++ Back-end/src/main/java/com/devsuperior/dsdelivey/DsdeliveyApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsdelivey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsdeliveyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsdeliveyApplication.class, args);
+	}
+
+}
---
+++ Back-end/src/test/java/com/devsuperior/dsdelivey/DsdeliveyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsdelivey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsdeliveyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
