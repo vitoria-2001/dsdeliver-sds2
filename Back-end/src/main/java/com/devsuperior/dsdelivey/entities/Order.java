2d0d128a00ed06ac1d8754d24b50739d089134b5
Vitória Amorim <vitoria.amorimm01@gmail.com>
2021-01-06 13:04:25 -0400

    POST /orders

--- Back-end/src/main/java/com/devsuperior/dsdelivey/entities/Order.java
+++ Back-end/src/main/java/com/devsuperior/dsdelivey/entities/Order.java
@@ -38,15 +38,13 @@ public class Order implements Serializable {
 	 public Order() {
 	 }
 
-	public Order(Long id, String address, Double latitude, Double longitude, Instant moment, OrderStatus status,
-			Set<Product> products) {
+	public Order(Long id, String address, Double latitude, Double longitude, Instant moment, OrderStatus status) {
 		this.id = id;
 		this.address = address;
 		this.latitude = latitude;
 		this.longitude = longitude;
 		this.moment = moment;
 		this.status = status;
-		this.products = products;
 	}
 
 	public Long getId() {
--- Back-end/src/main/java/com/devsuperior/dsdelivey/services/OrderService.java
+++ Back-end/src/main/java/com/devsuperior/dsdelivey/services/OrderService.java
@@ -1,5 +1,6 @@
 package com.devsuperior.dsdelivey.services;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -8,8 +9,12 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.dsdelivey.dto.OrderDTO;
+import com.devsuperior.dsdelivey.dto.ProductDTO;
 import com.devsuperior.dsdelivey.entities.Order;
+import com.devsuperior.dsdelivey.entities.OrderStatus;
+import com.devsuperior.dsdelivey.entities.Product;
 import com.devsuperior.dsdelivey.repositories.OrderRepository;
+import com.devsuperior.dsdelivey.repositories.ProductRepository;
 
 
 @Service
@@ -17,12 +22,25 @@ public class OrderService {
 	
 	@Autowired
 	private OrderRepository repository;
+	
+	@Autowired
+	private ProductRepository productRepository;
 
 	@Transactional (readOnly=true)
 	public List<OrderDTO> findAll() {
 		List<Order> list = repository.findOrderWithProducts();
 		return list.stream().map(x->new OrderDTO(x)).collect(Collectors.toList());
-		
-		
 	}
+	
+	@Transactional 
+	public OrderDTO insert (OrderDTO dto) {
+		Order order = new Order(null, dto.getAddress(), dto.getLatitude(), dto.getLongitude(),
+				Instant.now(), OrderStatus.PENDING);
+		for (ProductDTO p : dto.getProducts()) {
+			Product product = productRepository.getOne(p.getId());
+			order.getProducts().add(product);
+		}	
+		order=repository.save(order);
+		return new OrderDTO(order);	
+	 	}
 }
