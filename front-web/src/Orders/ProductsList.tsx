a33e1dd4aa45c542005c565e1b5f3b87027dfcbe
Vitória Amorim <vitoria.amorimm01@gmail.com>
2021-01-08 14:33:00 -0400

    Search order location

---
+++ front-web/src/Orders/ProductsList.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import './styles.css';
+import ProductCard from "./ProductCard";
+import { Product} from "./types";
+
+type Props = {
+    products:Product[];
+}
+
+function ProductsList({products}:Props) {
+    return (
+        <div className="orders-list-container">
+            <div className="orders-list-items">
+                {products.map(product=> (
+                    <ProductCard key={product.id} product={product}/>
+                ))}
+            </div>
+        </div>
+    )
+}
+
+export default ProductsList;
