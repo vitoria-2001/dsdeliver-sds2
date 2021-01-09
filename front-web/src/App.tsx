d0e32c9ea1a8da5f421cf206f003b029572a2126
Vitória Amorim <vitoria.amorimm01@gmail.com>
2021-01-09 12:49:11 -0400

    Solve error (netlify II)

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,4 +1,4 @@
-import { ToastContainer, toast } from 'react-toastify';
+import { ToastContainer} from 'react-toastify';
 import React from "react";
 import './App.css';
 import Routes from './Routes';
